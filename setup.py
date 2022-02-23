1c405d04a5941812a996dcaa5c6c9ce285dcc997
Addalin <60094638@cna-qatar.edu.qa>
2022-02-23 10:11:37 +0300

    Added and updated files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='An example python package with functions simulating depositing and withdrawing cash from a bank account',
     long_description=open('README.md').read(),
     install_requires=[],
-    url='REPOSITORY_URL',
-    author='AUTHOR_NAME',
-    author_email='AUTHOR_EMAIL'
+    url='https://github.com/Addalin-CP3445/Python_Package.git',
+    author='Addalin',
+    author_email='60094638@cna-qatar.edu.qa'
 )
